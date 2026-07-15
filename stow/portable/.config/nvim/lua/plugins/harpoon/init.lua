b1f2bbcf90142e21e62d65710f234c3cb16d9d8c
Nathan Ziabek <nathan434@zaizitri.com>
2026-07-15 17:03:13 +0100

    update

--- stow/portable/.config/nvim/lua/plugins/harpoon/init.lua
+++ stow/portable/.config/nvim/lua/plugins/harpoon/init.lua
@@ -11,38 +11,38 @@ return {
 
     vim.keymap.set("n", "<leader>a", function()
       harpoon:list():add()
-    end)
+    end, { desc = "Add file to Harpoon" })
 
     vim.keymap.set("n", "<C-e>", function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+    end, { desc = "Harpoon menu" })
 
     vim.keymap.set("n", "<C-h>", function()
       harpoon:list():prev()
-    end)
+    end, { desc = "Previous Harpoon file" })
 
     vim.keymap.set("n", "<C-l>", function()
       harpoon:list():next()
-    end)
+    end, { desc = "Next Harpoon file" })
 
     vim.keymap.set("n", "<leader>1", function()
       harpoon:list():select(1)
-    end)
+    end, { desc = "Harpoon file 1" })
 
     vim.keymap.set("n", "<leader>2", function()
       harpoon:list():select(2)
-    end)
+    end, { desc = "Harpoon file 2" })
 
     vim.keymap.set("n", "<leader>3", function()
       harpoon:list():select(3)
-    end)
+    end, { desc = "Harpoon file 3" })
 
     vim.keymap.set("n", "<leader>4", function()
       harpoon:list():select(4)
-    end)
+    end, { desc = "Harpoon file 4" })
 
     vim.keymap.set("n", "<leader>5", function()
       harpoon:list():select(5)
-    end)
+    end, { desc = "Harpoon file 5" })
   end,
 }
